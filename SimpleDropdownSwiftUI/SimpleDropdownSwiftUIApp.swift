617527eec89761a50b49657895d307b1f6364205
nurkholis <nur.kholis@ist.id>
2022-12-10 12:54:28 +0700

    Initial Commit

---
+++ SimpleDropdownSwiftUI/SimpleDropdownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDropdownSwiftUIApp.swift
+//  SimpleDropdownSwiftUI
+//
+//  Created by nur kholis on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDropdownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
